c6a0f3add5fae85eb78b09422c946e4169548b58
JonelSV <svjonel@gmail.com>
2021-08-17 14:38:01 -0400

    fixed more errors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let computerScore = 1;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result")
+const result_p = document.querySelector(".result > p")
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
@@ -24,33 +24,42 @@ function convert(letter) {
 
 
 function win(userChoice, computerChoice) {
-    
+    const smallUserWord = "user".fontsize(3).sup();
+    const smallCompWord = "comp".fontsize(3).sup();
+    const userGlow = document.getElementById(userChoice)
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sup();
-    const smallCompWord = "comp".fontsize(3).sup();
     result_p.innerHTML = `${convert(userChoice)} ${smallUserWord}  beats ${convert(computerChoice)} ${smallCompWord}  'You win'`;
+    userGlow.classList.add('glow1');
+    setTimeout(function(){userGlow.classList.remove('glow1')}, 500)
+
 }
 
 function lose(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const userGlow = document.getElementById(userChoice)
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sup();
-    const smallCompWord = "comp".fontsize(3).sup();
     result_p.innerHTML = `${convert(userChoice)} ${smallUserWord}  loses to ${convert(computerChoice)} ${smallCompWord}  'You lost'`;
-   
+    userGlow.classList.add('glow2');
+    setTimeout(function(){userGlow.classList.remove('glow2')}, 500)
+
 }
 
 
 function draw(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const userGlow = document.getElementById(userChoice)
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sup();
-    const smallCompWord = "comp".fontsize(3).sup();
     result_p.innerHTML = `${convert(userChoice)} ${smallUserWord}  equals ${convert(computerChoice)} ${smallCompWord}  'It's a draw'`;
-   
+    userGlow.classList.add('glow3');
+    setTimeout(function(){userGlow.classList.remove('glow3')}, 500)
+
 }
 
 
